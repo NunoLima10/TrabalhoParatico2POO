f9d39da98fdc1b2ca7d96e5c0a3c659b57a0b020
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-04-14 15:45:25 -0100

    Problema 0 completo

--- Problema0Robot/src/Robot.java
+++ Problema0Robot/src/Robot.java
@@ -1,2 +1,66 @@
+enum Directions {
+    NORTH, EAST, SOUTH, WEST;
+
+    public  static Directions getDirectionByOrdinal(int directionIndex) {
+        if (directionIndex < 0) directionIndex = 3;
+        if (directionIndex > 3) directionIndex = 0;
+
+        return Directions.values()[directionIndex];
+    }
+}
+
 public class Robot {
+    private double xPosition;
+    private double yPosition;
+    private Directions direction;
+
+    public Robot(double xPosition, double yPosition, Directions direction) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.direction = direction;
+    }
+
+    public void turnLeft() {
+        int directionIndex = this.direction.ordinal();
+        int newDirectionIndex = directionIndex - 1;
+        this.direction = Directions.getDirectionByOrdinal(newDirectionIndex);
+    }
+
+    public void turnRight() {
+        int directionIndex = this.direction.ordinal();
+        int newDirectionIndex = directionIndex + 1;
+        this.direction = Directions.getDirectionByOrdinal(newDirectionIndex);
+    }
+
+    public void  move() {
+        int directionIndex = this.direction.ordinal();
+
+        switch (directionIndex){
+            case 0:
+                this.yPosition++;
+                break;
+            case  1:
+                this.xPosition++;
+                break;
+            case 2:
+                this.yPosition--;
+                break;
+            case 3:
+                this.xPosition--;
+                break;
+        }
+    }
+
+    public void  move(int times) {
+        for (int i = 0; i <times ; i++) { this.move(); }
+    }
+
+    public double[] getLocation(){
+        return new double[] { this.xPosition, this.yPosition };
+    }
+    public String getDirection(){
+        String directionLabel = this.direction.toString();
+        return directionLabel.substring(0,1);
+    }
+
 }
--- Problema0Robot/src/main.java
+++ Problema0Robot/src/main.java
@@ -1,5 +1,18 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Robot robot = new Robot(0,0,Directions.EAST);
+        robot.move(3);
+        robot.turnLeft();
+        robot.move(3);
+
+        double [] location = robot.getLocation();
+        System.out.println(location[0]);
+        System.out.println(location[1]);
+
+        System.out.println(robot.getDirection());
+
+
+
     }
 }
